0bae8c0849d5528c0b9eb853d02f9a22221f2b91
Louis Waweru <waweru@gmail.com>
2025-10-07 09:20:52 -0400

    Adds support for patching qBittorrent in:  - Linux (GNU/Linux; Flatpak; SnapStore)  - macOS (qBittorrent v3.3.7+)

--- IpFilterLib/Services/QBittorrentPatchClient.cs
+++ IpFilterLib/Services/QBittorrentPatchClient.cs
@@ -3,33 +3,87 @@
 namespace IpFilterLib.Services;
 public class QBittorrentPatchClient : IPatchClient
 {
+    private string _iniPath = string.Empty;
+    readonly string[] _linuxPaths = [
+        ".config/qBittorrent/",
+        ".var/app/org.qbittorrent.qBittorrent/config/qBittorrent/",
+        "snap/qbittorrent-arnatious/current/.config/qBittorrent/" ];
+
     public string Name => "qBittorrent";
+
 
     public bool SettingsExist()
     {
-        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        string iniPath = Path.Combine(appData, "qBittorrent", "qBittorrent.ini");
-        return File.Exists(iniPath);
+        if(OperatingSystem.IsWindows())
+        {
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            _iniPath = Path.Combine(appData, $"{Name}", $"{Name}.ini");
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            int configsFound = 0;
+
+            foreach (string path in _linuxPaths)
+            {
+                _iniPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                                        path,
+                                        $"{Name}",
+                                        $"{Name}.ini");
+                if (File.Exists(_iniPath))
+                    configsFound++;
+            }
+
+            if (configsFound > 0)
+                return true;
+        }
+        else if (OperatingSystem.IsMacOS())
+            _iniPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                                    ".config",
+                                    $"{Name}",
+                                    $"{Name}.ini");
+
+        return File.Exists(_iniPath); 
     }
 
     public bool TryPatchSettings(string ipFilterPath)
     {
-        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        string iniPath = Path.Combine(appData, Name, $"{Name}.ini");
+        if (OperatingSystem.IsLinux())
+        {
+            string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            foreach (string path in _linuxPaths)
+                if (File.Exists(Path.Combine(userProfile, path, $"{Name}", $"{Name}.ini")))
+                    try { PatchSettings(ipFilterPath); }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+        } 
+        else if (OperatingSystem.IsMacOS())
+            _iniPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                                    ".config",
+                                    $"{Name}",
+                                    $"{Name}.ini");
+        else if (OperatingSystem.IsWindows())
+            _iniPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                                    $"{Name}",
+                                    $"{Name}.ini");
 
-        if (!File.Exists(iniPath))
-            return false;
+        if (OperatingSystem.IsMacOS() || OperatingSystem.IsWindows())
+            try { PatchSettings(ipFilterPath); }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
 
-        List<string>? lines = File.ReadAllLines(iniPath).ToList();
+        return true;
+    }
+
+    public void PatchSettings(string ipFilterPath)
+    {
+        List<string>? lines = File.ReadAllLines(_iniPath).ToList();
         int sectionIndex = lines.FindIndex(line => line.Trim() == "[BitTorrent]");
         if (sectionIndex == -1)
-            return false;
+            return;
 
-        lines.RemoveAll(line => line.StartsWith("IPFilter\\"));
-        lines.Insert(sectionIndex + 1, $"Session\\IPFilteringEnabled=true");
+        lines.RemoveAll(line => line.StartsWith(@"IPFilter\"));
+        lines.RemoveAll(line => line.StartsWith(@"Session\IPFilter"));
+        lines.Insert(sectionIndex + 1, @"Session\IPFilteringEnabled=true");
         lines.Insert(sectionIndex + 2, $"Session\\IPFilter={Path.GetFullPath(ipFilterPath).Replace(@"\", @"\\")}");
 
-        File.WriteAllLines(iniPath, lines);
-        return true;
+        File.WriteAllLines(_iniPath, lines);
     }
 }
